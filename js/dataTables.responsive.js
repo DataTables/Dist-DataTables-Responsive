8db1dbc4298f344f981e5e33146efcf852180c47
Allan Jardine <allan@datatables.net>
2022-11-04 14:52:49 +0000

    Missing files in DataTables-DataTables

--- js/dataTables.responsive.js
+++ js/dataTables.responsive.js
@@ -1,6 +1,47 @@
+
 /*! Responsive 2.4.0-dev
  * 2014-2022 SpryMedia Ltd - datatables.net/license
  */
+
+(function( factory ){
+	if ( typeof define === 'function' && define.amd ) {
+		// AMD
+		define( ['jquery', 'datatables.net'], function ( $ ) {
+			return factory( $, window, document );
+		} );
+	}
+	else if ( typeof exports === 'object' ) {
+		// CommonJS
+		module.exports = function (root, $) {
+			if ( ! root ) {
+				// CommonJS environments without a window global must pass a
+				// root. This will give an error otherwise
+				root = window;
+			}
+
+			if ( ! $ ) {
+				$ = typeof window !== 'undefined' ? // jQuery's factory checks for a global window
+					require('jquery') :
+					require('jquery')( root );
+			}
+
+			if ( ! $.fn.dataTable ) {
+				require('datatables.net')(root, $);
+			}
+
+
+			return factory( $, root, root.document );
+		};
+	}
+	else {
+		// Browser
+		factory( jQuery, window, document );
+	}
+}(function( $, window, document, undefined ) {
+'use strict';
+var DataTable = $.fn.dataTable;
+
+
 
 /**
  * @summary     Responsive
@@ -1480,3 +1521,7 @@ $(document).on( 'preInit.dt.dtr', function (e, settings, json) {
 		}
 	}
 } );
+
+
+return DataTable;
+}));
