7892d432d3711df4147426faca290c3635823c98
dtbuild <dtbuild@rigil>
2024-08-30 13:26:27 +0000

    935c1f4efc8aa7b8c63e83921a693fe806279e36 Release 3.0.3
    Sync to source repo @935c1f4efc8aa7b8c63e83921a693fe806279e36

--- js/dataTables.responsive.js
+++ js/dataTables.responsive.js
@@ -1,4 +1,4 @@
-/*! Responsive 3.0.2
+/*! Responsive 3.0.3
  * © SpryMedia Ltd - datatables.net/license
  */
 
@@ -52,7 +52,7 @@ var DataTable = $.fn.dataTable;
 /**
  * @summary     Responsive
  * @description Responsive tables plug-in for DataTables
- * @version     3.0.2
+ * @version     3.0.3
  * @author      SpryMedia Ltd
  * @copyright   SpryMedia Ltd.
  *
@@ -1809,7 +1809,7 @@ Api.registerPlural(
  * @name Responsive.version
  * @static
  */
-Responsive.version = '3.0.2';
+Responsive.version = '3.0.3';
 
 $.fn.dataTable.Responsive = Responsive;
 $.fn.DataTable.Responsive = Responsive;
